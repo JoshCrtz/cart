38b73ca23711ee09b217a8d311f5a4c9be99ea2f
Joshua Cortez <cortez.joshuachristian@auf.edu.ph>
2022-04-07 20:29:13 +0800

    Customized the design, added my own logo, updated the layout of products, and shopping cart

--- products-list.php
+++ products-list.php
@@ -9,13 +9,33 @@ $products = Product::convertArrayToProducts($products_data);
 
 $customer = new Customer('John Doe', 'john@mail.com');
 ?>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
+<meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+            <link rel="stylesheet" href="Style.css">
+
     <title>My Cart</title>
 </head>
+
 <body>
 
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">
+        <div class="prodList">
+            <h1>Welcome <?php echo $customer->getName() ?>!</h1>
+        </div>
+    </span>
+  </div>
+</nav>
+
+
+
 <h2>Products</h2>
 <h4>
     <a href="shopping-cart.php">Shopping Cart</a>
